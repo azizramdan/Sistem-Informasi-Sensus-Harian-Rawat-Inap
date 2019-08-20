301e6429dab4c7e0f432fc37a64b0a67e690670f
azizramdan <azizramdan44@gmail.com>
2019-08-20 11:17:34 +0700

    fix login

--- Sistem Informasi Sensus Harian Rawat Inap/Login.Designer.vb
+++ Sistem Informasi Sensus Harian Rawat Inap/Login.Designer.vb
@@ -22,42 +22,42 @@ Partial Class Login
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.tbUsername = New System.Windows.Forms.TextBox()
+        Me.tbPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'tbUsername
         '
-        Me.TextBox1.Location = New System.Drawing.Point(107, 83)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.tbUsername.Location = New System.Drawing.Point(107, 83)
+        Me.tbUsername.Name = "tbUsername"
+        Me.tbUsername.Size = New System.Drawing.Size(100, 20)
+        Me.tbUsername.TabIndex = 0
         '
-        'TextBox2
+        'tbPassword
         '
-        Me.TextBox2.Location = New System.Drawing.Point(107, 115)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 1
+        Me.tbPassword.Location = New System.Drawing.Point(107, 115)
+        Me.tbPassword.Name = "tbPassword"
+        Me.tbPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.tbPassword.Size = New System.Drawing.Size(100, 20)
+        Me.tbPassword.TabIndex = 1
         '
-        'Button1
+        'btnLogin
         '
-        Me.Button1.Location = New System.Drawing.Point(91, 162)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Login"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnLogin.Location = New System.Drawing.Point(107, 160)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 2
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(88, 36)
+        Me.Label1.Location = New System.Drawing.Point(95, 35)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(75, 13)
         Me.Label1.TabIndex = 3
@@ -81,7 +81,7 @@ Partial Class Login
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Password"
         '
-        'Form1
+        'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
@@ -89,18 +89,18 @@ Partial Class Login
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Name = "Form1"
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.tbPassword)
+        Me.Controls.Add(Me.tbUsername)
+        Me.Name = "Login"
         Me.Text = "Login Petugas"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents tbUsername As System.Windows.Forms.TextBox
+    Friend WithEvents tbPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
--- Sistem Informasi Sensus Harian Rawat Inap/Login.vb
+++ Sistem Informasi Sensus Harian Rawat Inap/Login.vb
@@ -1,3 +1,38 @@
-﻿Public Class Login
+﻿Imports System.Data.OleDb
 
+Public Class Login
+    Private Sub Login_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        connect()
+        Dim akun As String = "admin1"
+        tbUsername.Text = akun
+        tbPassword.Text = akun
+    End Sub
+
+    Private Sub btnLogin_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLogin.Click
+        If tbUsername.Text = "" Or tbPassword.Text = "" Then
+            MsgBox("Username dan Password harus diisi!")
+        Else
+            Dim source As String = My.Settings.DBConnectionString
+            Dim conn = New OleDbConnection(source)
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+                Dim username As String = tbUsername.Text
+                Dim password As String = tbPassword.Text
+                Dim query As String = "SELECT id FROM [user] WHERE username=@username AND password=@password"
+                Dim cmd As New OleDbCommand(query, conn)
+                cmd.Parameters.AddWithValue("@username", username)
+                cmd.Parameters.AddWithValue("@password", password)
+                Dim result As OleDbDataReader = cmd.ExecuteReader
+                If result.Read Then
+                    Dim form As New MenuForm
+                    form.Show()
+                    Me.Close()
+                Else
+                    MsgBox("Login gagal")
+                End If
+            Else
+                MsgBox("Koneksi database gagal!")
+            End If
+        End If
+    End Sub
 End Class
