3fffd7b5b32764905539b69679ba7ce8e76d1951
azizramdan <azizramdan44@gmail.com>
2019-08-20 11:16:45 +0700

    add module connection

---
+++ Sistem Informasi Sensus Harian Rawat Inap/Connection.vb
@@ -0,0 +1,7 @@
+﻿Module Connection
+    Sub connect()
+        Dim path As String = AppDomain.CurrentDomain.BaseDirectory
+        path = path.Replace("bin\Debug\", "SISHRI.accdb")
+        My.Settings("DBConnectionString") = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & path
+    End Sub
+End Module
