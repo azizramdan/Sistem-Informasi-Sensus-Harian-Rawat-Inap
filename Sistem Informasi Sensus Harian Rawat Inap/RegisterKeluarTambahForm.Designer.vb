6de811d61d562af32359edba6853e744aeb7601e
azizramdan <azizramdan44@gmail.com>
2019-08-21 17:31:52 +0700

    add tambah register keluar

--- Sistem Informasi Sensus Harian Rawat Inap/RegisterKeluarTambahForm.Designer.vb
+++ Sistem Informasi Sensus Harian Rawat Inap/RegisterKeluarTambahForm.Designer.vb
@@ -23,43 +23,49 @@ Partial Class RegisterKeluarTambahForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.tbMedrec = New System.Windows.Forms.TextBox()
+        Me.tbNama = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
-        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.dtpTanggal = New System.Windows.Forms.DateTimePicker()
+        Me.cbCaraKeluar = New System.Windows.Forms.ComboBox()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.btnBersihkan = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbId = New System.Windows.Forms.TextBox()
+        Me.btnCek = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Location = New System.Drawing.Point(12, 35)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(63, 13)
         Me.Label1.TabIndex = 10
         Me.Label1.Text = "No. Medrec"
         '
-        'TextBox1
+        'tbMedrec
         '
-        Me.TextBox1.Location = New System.Drawing.Point(129, 6)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(181, 20)
-        Me.TextBox1.TabIndex = 8
+        Me.tbMedrec.Enabled = False
+        Me.tbMedrec.Location = New System.Drawing.Point(129, 32)
+        Me.tbMedrec.Name = "tbMedrec"
+        Me.tbMedrec.Size = New System.Drawing.Size(181, 20)
+        Me.tbMedrec.TabIndex = 8
         '
-        'TextBox2
+        'tbNama
         '
-        Me.TextBox2.Location = New System.Drawing.Point(129, 32)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(181, 20)
-        Me.TextBox2.TabIndex = 9
+        Me.tbNama.Enabled = False
+        Me.tbNama.Location = New System.Drawing.Point(129, 58)
+        Me.tbNama.Name = "tbNama"
+        Me.tbNama.Size = New System.Drawing.Size(181, 20)
+        Me.tbNama.TabIndex = 9
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 35)
+        Me.Label2.Location = New System.Drawing.Point(12, 61)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(70, 13)
         Me.Label2.TabIndex = 11
@@ -68,7 +74,7 @@ Partial Class RegisterKeluarTambahForm
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(12, 87)
+        Me.Label6.Location = New System.Drawing.Point(12, 113)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(97, 13)
         Me.Label6.TabIndex = 15
@@ -77,63 +83,106 @@ Partial Class RegisterKeluarTambahForm
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(12, 64)
+        Me.Label5.Location = New System.Drawing.Point(12, 90)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(79, 13)
         Me.Label5.TabIndex = 14
         Me.Label5.Text = "Tanggal Keluar"
         '
-        'DateTimePicker1
+        'dtpTanggal
         '
-        Me.DateTimePicker1.Location = New System.Drawing.Point(129, 58)
-        Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(181, 20)
-        Me.DateTimePicker1.TabIndex = 12
+        Me.dtpTanggal.Location = New System.Drawing.Point(129, 84)
+        Me.dtpTanggal.Name = "dtpTanggal"
+        Me.dtpTanggal.Size = New System.Drawing.Size(181, 20)
+        Me.dtpTanggal.TabIndex = 12
         '
-        'ComboBox3
+        'cbCaraKeluar
         '
-        Me.ComboBox3.FormattingEnabled = True
-        Me.ComboBox3.Location = New System.Drawing.Point(129, 84)
-        Me.ComboBox3.Name = "ComboBox3"
-        Me.ComboBox3.Size = New System.Drawing.Size(181, 21)
-        Me.ComboBox3.TabIndex = 13
+        Me.cbCaraKeluar.FormattingEnabled = True
+        Me.cbCaraKeluar.Items.AddRange(New Object() {"Dipulangkan", "Permintaan sendiri", "Melarikan diri", "Dirujuk"})
+        Me.cbCaraKeluar.Location = New System.Drawing.Point(129, 110)
+        Me.cbCaraKeluar.Name = "cbCaraKeluar"
+        Me.cbCaraKeluar.Size = New System.Drawing.Size(181, 21)
+        Me.cbCaraKeluar.TabIndex = 13
         '
-        'Button1
+        'btnTambah
         '
-        Me.Button1.Location = New System.Drawing.Point(98, 121)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 16
-        Me.Button1.Text = "Tambah"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnTambah.Location = New System.Drawing.Point(79, 148)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(75, 23)
+        Me.btnTambah.TabIndex = 16
+        Me.btnTambah.Text = "Tambah"
+        Me.btnTambah.UseVisualStyleBackColor = True
         '
-        'TambahRegisterKeluarForm
+        'btnBersihkan
+        '
+        Me.btnBersihkan.Location = New System.Drawing.Point(173, 148)
+        Me.btnBersihkan.Name = "btnBersihkan"
+        Me.btnBersihkan.Size = New System.Drawing.Size(75, 23)
+        Me.btnBersihkan.TabIndex = 43
+        Me.btnBersihkan.Text = "Bersihkan"
+        Me.btnBersihkan.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 13)
+        Me.Label3.TabIndex = 44
+        Me.Label3.Text = "ID Register Masuk"
+        '
+        'tbId
+        '
+        Me.tbId.Location = New System.Drawing.Point(129, 6)
+        Me.tbId.Name = "tbId"
+        Me.tbId.Size = New System.Drawing.Size(138, 20)
+        Me.tbId.TabIndex = 45
+        '
+        'btnCek
+        '
+        Me.btnCek.Location = New System.Drawing.Point(273, 4)
+        Me.btnCek.Name = "btnCek"
+        Me.btnCek.Size = New System.Drawing.Size(38, 23)
+        Me.btnCek.TabIndex = 46
+        Me.btnCek.Text = "Cek"
+        Me.btnCek.UseVisualStyleBackColor = True
+        '
+        'RegisterKeluarTambahForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(323, 158)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(326, 183)
+        Me.Controls.Add(Me.btnCek)
+        Me.Controls.Add(Me.tbId)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnBersihkan)
+        Me.Controls.Add(Me.btnTambah)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.DateTimePicker1)
-        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.dtpTanggal)
+        Me.Controls.Add(Me.cbCaraKeluar)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.tbMedrec)
+        Me.Controls.Add(Me.tbNama)
         Me.Controls.Add(Me.Label2)
-        Me.Name = "TambahRegisterKeluarForm"
+        Me.Name = "RegisterKeluarTambahForm"
         Me.Text = "Tambah Data Pasien Keluar Rawat Inap"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents tbMedrec As System.Windows.Forms.TextBox
+    Friend WithEvents tbNama As System.Windows.Forms.TextBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label6 As System.Windows.Forms.Label
     Friend WithEvents Label5 As System.Windows.Forms.Label
-    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
-    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
-    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents dtpTanggal As System.Windows.Forms.DateTimePicker
+    Friend WithEvents cbCaraKeluar As System.Windows.Forms.ComboBox
+    Friend WithEvents btnTambah As System.Windows.Forms.Button
+    Friend WithEvents btnBersihkan As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents tbId As System.Windows.Forms.TextBox
+    Friend WithEvents btnCek As System.Windows.Forms.Button
 End Class
--- Sistem Informasi Sensus Harian Rawat Inap/RegisterKeluarTambahForm.vb
+++ Sistem Informasi Sensus Harian Rawat Inap/RegisterKeluarTambahForm.vb
@@ -1,3 +1,89 @@
-﻿Public Class RegisterKeluarTambahForm
+﻿Imports System.Data.OleDb
+Public Class RegisterKeluarTambahForm
+    Dim btnCek_Clicked As Boolean
+    Private Sub RegisterKeluarTambahForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        connect()
+    End Sub
 
+    Private Sub Form_Closing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
+        If e.CloseReason = CloseReason.UserClosing Then
+            RegisterKeluarForm.Show()
+            e.Cancel = False
+        End If
+    End Sub
+
+    Private Sub btnCek_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCek.Click
+        If btnCek_Clicked Then
+            btnCek_Clicked = False
+            btnCek.Text = "Cek"
+            tbId.Enabled = True
+            tbId.Text = ""
+            tbMedrec.Text = ""
+            tbNama.Text = ""
+        End If
+        Dim id As String = tbId.Text
+        Dim source As String = My.Settings.DBConnectionString
+        Dim conn = New OleDbConnection(source)
+        If conn.State = ConnectionState.Closed Then
+            conn.Open()
+            Dim query As String = "SELECT rm.no_medrec, p.nama_lengkap FROM [register_masuk] AS rm, [pasien] AS p WHERE rm.id=@id AND p.no_medrec = rm.no_medrec"
+            Dim cmd As New OleDbCommand(query, conn)
+            cmd.Parameters.AddWithValue("@id", id)
+            Dim result As OleDbDataReader = cmd.ExecuteReader
+            If result.Read Then
+                tbMedrec.Text = result.GetString(0)
+                tbNama.Text = result.GetString(1)
+                btnCek_Clicked = True
+                btnCek.Text = "X"
+                tbId.Enabled = False
+            End If
+        Else
+            MsgBox("Koneksi database gagal!")
+        End If
+    End Sub
+
+    Private Sub btnTambah_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTambah.Click
+        Dim id, tanggalKeluar, caraKeluar As String
+        id = tbId.Text
+        tanggalKeluar = dtpTanggal.Value.ToString("M/d/yyyy")
+        caraKeluar = cbCaraKeluar.SelectedItem
+
+        If Not btnCek_Clicked Then
+            MsgBox("Cek ID Register Masuk terlebih dahulu!")
+        ElseIf tanggalKeluar = "" Or caraKeluar = "" Then
+            MsgBox("Data harus diisi semua")
+        Else
+            Dim source As String = My.Settings.DBConnectionString
+            Dim conn = New OleDbConnection(source)
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+                Dim query As String = "INSERT INTO register_keluar (id_register_masuk, tanggal_keluar, cara_pasien_keluar) "
+                query &= "VALUES (@id, @tanggalKeluar, @caraKeluar)"
+                Dim cmd As New OleDbCommand(query, conn)
+                cmd.Parameters.AddWithValue("@id", id)
+                cmd.Parameters.AddWithValue("@tanggalKeluar", tanggalKeluar)
+                cmd.Parameters.AddWithValue("@caraKeluar", caraKeluar)
+                Dim result As Integer = cmd.ExecuteNonQuery
+                If result > 0 Then
+                    MsgBox("Data berhasil disimpan")
+                Else
+                    MsgBox("Data gagal disimpan")
+                End If
+            Else
+                MsgBox("Koneksi database gagal!")
+            End If
+        End If
+    End Sub
+
+    Private Sub btnBersihkan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBersihkan.Click
+        If btnCek_Clicked Then
+            btnCek_Clicked = False
+            btnCek.Text = "Cek"
+            tbId.Enabled = True
+        End If
+        tbId.Text = ""
+        tbMedrec.Text = ""
+        tbNama.Text = ""
+        cbCaraKeluar.SelectedIndex = -1
+    End Sub
 End Class
