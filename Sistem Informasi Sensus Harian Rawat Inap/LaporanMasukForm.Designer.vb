8ccebdf3167d3b61ac55ccbade5984f53f638c77
azizramdan <azizramdan44@gmail.com>
2019-08-27 11:07:50 +0700

    create laporan masuk

---
+++ Sistem Informasi Sensus Harian Rawat Inap/LaporanMasukForm.Designer.vb
@@ -0,0 +1,327 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LaporanMasukForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.rbBulanan = New System.Windows.Forms.RadioButton()
+        Me.rbHarian = New System.Windows.Forms.RadioButton()
+        Me.btnCari = New System.Windows.Forms.Button()
+        Me.cbRuangan = New System.Windows.Forms.ComboBox()
+        Me.cbKelas = New System.Windows.Forms.ComboBox()
+        Me.dtpTanggal = New System.Windows.Forms.DateTimePicker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DBDataSet = New Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSet()
+        Me.Register_masuk_QueryBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Register_masuk_QueryTableAdapter = New Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSetTableAdapters.register_masuk_QueryTableAdapter()
+        Me.TableAdapterManager = New Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSetTableAdapters.TableAdapterManager()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Register_masuk_QueryDataGridView = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn10 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn11 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DBDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Register_masuk_QueryBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.Register_masuk_QueryDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(374, 77)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrint.TabIndex = 21
+        Me.btnPrint.Text = "Print"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(371, 45)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(33, 13)
+        Me.Label9.TabIndex = 22
+        Me.Label9.Text = "Kelas"
+        '
+        'rbBulanan
+        '
+        Me.rbBulanan.AutoSize = True
+        Me.rbBulanan.Location = New System.Drawing.Point(233, 19)
+        Me.rbBulanan.Name = "rbBulanan"
+        Me.rbBulanan.Size = New System.Drawing.Size(52, 17)
+        Me.rbBulanan.TabIndex = 14
+        Me.rbBulanan.TabStop = True
+        Me.rbBulanan.Text = "Bulan"
+        Me.rbBulanan.UseVisualStyleBackColor = True
+        '
+        'rbHarian
+        '
+        Me.rbHarian.AutoSize = True
+        Me.rbHarian.Location = New System.Drawing.Point(139, 19)
+        Me.rbHarian.Name = "rbHarian"
+        Me.rbHarian.Size = New System.Drawing.Size(56, 17)
+        Me.rbHarian.TabIndex = 12
+        Me.rbHarian.TabStop = True
+        Me.rbHarian.Text = "Harian"
+        Me.rbHarian.UseVisualStyleBackColor = True
+        '
+        'btnCari
+        '
+        Me.btnCari.Location = New System.Drawing.Point(264, 77)
+        Me.btnCari.Name = "btnCari"
+        Me.btnCari.Size = New System.Drawing.Size(75, 23)
+        Me.btnCari.TabIndex = 20
+        Me.btnCari.Text = "Cari"
+        Me.btnCari.UseVisualStyleBackColor = True
+        '
+        'cbRuangan
+        '
+        Me.cbRuangan.FormattingEnabled = True
+        Me.cbRuangan.Items.AddRange(New Object() {"Berlian Barat", "Berlian Timur", "Safir Barat", "Safir Timur", "Ruby Barat", "Ruby Timur", "Topas", "Maternity", "Mutiara", "Intermediate", "Nursery", "Perina"})
+        Me.cbRuangan.Location = New System.Drawing.Point(431, 15)
+        Me.cbRuangan.Name = "cbRuangan"
+        Me.cbRuangan.Size = New System.Drawing.Size(164, 21)
+        Me.cbRuangan.TabIndex = 18
+        '
+        'cbKelas
+        '
+        Me.cbKelas.FormattingEnabled = True
+        Me.cbKelas.Items.AddRange(New Object() {"President Suite", "Suite", "Junior Suite", "Premiere", "Kelas I", "Kelas II", "Kelas III", "Isolasi"})
+        Me.cbKelas.Location = New System.Drawing.Point(431, 42)
+        Me.cbKelas.Name = "cbKelas"
+        Me.cbKelas.Size = New System.Drawing.Size(164, 21)
+        Me.cbKelas.TabIndex = 19
+        '
+        'dtpTanggal
+        '
+        Me.dtpTanggal.Location = New System.Drawing.Point(139, 42)
+        Me.dtpTanggal.Name = "dtpTanggal"
+        Me.dtpTanggal.Size = New System.Drawing.Size(200, 20)
+        Me.dtpTanggal.TabIndex = 16
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(371, 18)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 13)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "Ruangan"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(78, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Tanggal"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(78, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Periode"
+        '
+        'DBDataSet
+        '
+        Me.DBDataSet.DataSetName = "DBDataSet"
+        Me.DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'Register_masuk_QueryBindingSource
+        '
+        Me.Register_masuk_QueryBindingSource.DataMember = "register_masuk Query"
+        Me.Register_masuk_QueryBindingSource.DataSource = Me.DBDataSet
+        '
+        'Register_masuk_QueryTableAdapter
+        '
+        Me.Register_masuk_QueryTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.Connection = Nothing
+        Me.TableAdapterManager.dokterTableAdapter = Nothing
+        Me.TableAdapterManager.kelasTableAdapter = Nothing
+        Me.TableAdapterManager.pasienTableAdapter = Nothing
+        Me.TableAdapterManager.register_keluarTableAdapter = Nothing
+        Me.TableAdapterManager.register_masukTableAdapter = Nothing
+        Me.TableAdapterManager.ruanganTableAdapter = Nothing
+        Me.TableAdapterManager.tempat_tidurTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        Me.TableAdapterManager.userTableAdapter = Nothing
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 477)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(691, 22)
+        Me.StatusStrip1.TabIndex = 24
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 17)
+        '
+        'Register_masuk_QueryDataGridView
+        '
+        Me.Register_masuk_QueryDataGridView.AllowUserToAddRows = False
+        Me.Register_masuk_QueryDataGridView.AllowUserToDeleteRows = False
+        Me.Register_masuk_QueryDataGridView.AutoGenerateColumns = False
+        Me.Register_masuk_QueryDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Register_masuk_QueryDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn8, Me.DataGridViewTextBoxColumn9, Me.DataGridViewTextBoxColumn10, Me.DataGridViewTextBoxColumn11})
+        Me.Register_masuk_QueryDataGridView.DataSource = Me.Register_masuk_QueryBindingSource
+        Me.Register_masuk_QueryDataGridView.Location = New System.Drawing.Point(12, 117)
+        Me.Register_masuk_QueryDataGridView.Name = "Register_masuk_QueryDataGridView"
+        Me.Register_masuk_QueryDataGridView.ReadOnly = True
+        Me.Register_masuk_QueryDataGridView.Size = New System.Drawing.Size(664, 357)
+        Me.Register_masuk_QueryDataGridView.TabIndex = 25
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "id"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "ID Register"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "no_medrec"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "No. Medrec"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "nama_lengkap"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Nama Lengkap"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        Me.DataGridViewTextBoxColumn3.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "tanggal_masuk"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Tanggal Masuk"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        Me.DataGridViewTextBoxColumn4.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn8
+        '
+        Me.DataGridViewTextBoxColumn8.DataPropertyName = "ruangan"
+        Me.DataGridViewTextBoxColumn8.HeaderText = "Ruangan"
+        Me.DataGridViewTextBoxColumn8.Name = "DataGridViewTextBoxColumn8"
+        Me.DataGridViewTextBoxColumn8.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn9
+        '
+        Me.DataGridViewTextBoxColumn9.DataPropertyName = "kelas"
+        Me.DataGridViewTextBoxColumn9.HeaderText = "Kelas"
+        Me.DataGridViewTextBoxColumn9.Name = "DataGridViewTextBoxColumn9"
+        Me.DataGridViewTextBoxColumn9.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn10
+        '
+        Me.DataGridViewTextBoxColumn10.DataPropertyName = "cara_pasien_masuk"
+        Me.DataGridViewTextBoxColumn10.HeaderText = "Cara Masuk"
+        Me.DataGridViewTextBoxColumn10.Name = "DataGridViewTextBoxColumn10"
+        Me.DataGridViewTextBoxColumn10.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn11
+        '
+        Me.DataGridViewTextBoxColumn11.DataPropertyName = "deleted_at"
+        Me.DataGridViewTextBoxColumn11.HeaderText = "deleted_at"
+        Me.DataGridViewTextBoxColumn11.Name = "DataGridViewTextBoxColumn11"
+        Me.DataGridViewTextBoxColumn11.ReadOnly = True
+        Me.DataGridViewTextBoxColumn11.Visible = False
+        '
+        'LaporanMasukForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(691, 499)
+        Me.Controls.Add(Me.Register_masuk_QueryDataGridView)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.rbBulanan)
+        Me.Controls.Add(Me.rbHarian)
+        Me.Controls.Add(Me.btnCari)
+        Me.Controls.Add(Me.cbRuangan)
+        Me.Controls.Add(Me.cbKelas)
+        Me.Controls.Add(Me.dtpTanggal)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "LaporanMasukForm"
+        Me.Text = "Laporan Pasien Masuk"
+        CType(Me.DBDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Register_masuk_QueryBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        CType(Me.Register_masuk_QueryDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnPrint As System.Windows.Forms.Button
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents rbBulanan As System.Windows.Forms.RadioButton
+    Friend WithEvents rbHarian As System.Windows.Forms.RadioButton
+    Friend WithEvents btnCari As System.Windows.Forms.Button
+    Friend WithEvents cbRuangan As System.Windows.Forms.ComboBox
+    Friend WithEvents cbKelas As System.Windows.Forms.ComboBox
+    Friend WithEvents dtpTanggal As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DBDataSet As Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSet
+    Friend WithEvents Register_masuk_QueryBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents Register_masuk_QueryTableAdapter As Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSetTableAdapters.register_masuk_QueryTableAdapter
+    Friend WithEvents TableAdapterManager As Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Register_masuk_QueryDataGridView As System.Windows.Forms.DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn8 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn9 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn10 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn11 As System.Windows.Forms.DataGridViewTextBoxColumn
+End Class
---
+++ Sistem Informasi Sensus Harian Rawat Inap/LaporanMasukForm.vb
@@ -0,0 +1,55 @@
+﻿Public Class LaporanMasukForm
+
+    Private Sub LaporanMasukForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        connect()
+    End Sub
+
+    Private Sub Form_Closing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
+        If e.CloseReason = CloseReason.UserClosing Then
+            MenuForm.Show()
+            e.Cancel = False
+        End If
+    End Sub
+
+    Private Sub rbHarian_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rbHarian.CheckedChanged
+        dtpTanggal.ShowUpDown = False
+        dtpTanggal.Format = DateTimePickerFormat.Custom
+        dtpTanggal.CustomFormat = "dddd, d MMMM yyyy"
+    End Sub
+
+    Private Sub rbBulanan_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rbBulanan.CheckedChanged
+        dtpTanggal.ShowUpDown = True
+        dtpTanggal.Format = DateTimePickerFormat.Custom
+        dtpTanggal.CustomFormat = "MMMM yyyy"
+    End Sub
+
+    Private Sub btnCari_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCari.Click
+        If cbRuangan.SelectedIndex = -1 Or cbKelas.SelectedIndex = -1 Then
+            MsgBox("Data harus diisi semua")
+        Else
+            Dim filter As String
+            If rbHarian.Checked Then
+                Dim tglMasuk As String
+                tglMasuk = dtpTanggal.Value.ToString("M/d/yyyy")
+                filter = "tanggal_masuk = #" & tglMasuk & "#"
+            Else
+                Dim awalBulan As Date = dtpTanggal.Value.Month & "/1/" & dtpTanggal.Value.Year
+                Dim akhirBulan As Date = awalBulan.AddMonths(1)
+
+                filter = "tanggal_masuk >= #" & awalBulan.ToString("M/d/yyyy") & "#"
+                filter &= " AND tanggal_masuk <= #" & akhirBulan.ToString("M/d/yyyy") & "#"
+            End If
+
+            filter &= " AND ruangan = '" & cbRuangan.SelectedItem & "'"
+            filter &= " AND kelas = '" & cbKelas.SelectedItem & "'"
+            filter &= " AND deleted_at IS NULL"
+            Register_masuk_QueryBindingSource.Filter = filter
+            Register_masuk_QueryBindingSource.Sort = "id"
+            Me.Register_masuk_QueryTableAdapter.Fill(Me.DBDataSet.register_masuk_Query)
+            Jumlah()
+        End If
+    End Sub
+    Private Sub Jumlah()
+        ToolStripStatusLabel1.Text = "Jumlah data " & Register_masuk_QueryDataGridView.RowCount
+    End Sub
+End Class
