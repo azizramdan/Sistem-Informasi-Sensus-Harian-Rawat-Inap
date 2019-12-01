9612f217205ac80bc3b0b25bfe7e0f0547506276
azizramdan <azizramdan44@gmail.com>
2019-12-01 12:46:59 +0700

    create form pilih dokter

---
+++ Sistem Informasi Sensus Harian Rawat Inap/PilihDokterForm.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PilihDokterForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DokterTableAdapter = New Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSetTableAdapters.dokterTableAdapter()
+        Me.TableAdapterManager = New Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSetTableAdapters.TableAdapterManager()
+        Me.DokterDataGridView = New System.Windows.Forms.DataGridView()
+        Me.DokterBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DBDataSet = New Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSet()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnPilih = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.tbCari = New System.Windows.Forms.TextBox()
+        Me.btnCari = New System.Windows.Forms.Button()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DokterDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DokterBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DBDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DokterTableAdapter
+        '
+        Me.DokterTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.dokterTableAdapter = Me.DokterTableAdapter
+        Me.TableAdapterManager.kelasTableAdapter = Nothing
+        Me.TableAdapterManager.pasienTableAdapter = Nothing
+        Me.TableAdapterManager.register_keluarTableAdapter = Nothing
+        Me.TableAdapterManager.register_masukTableAdapter = Nothing
+        Me.TableAdapterManager.ruanganTableAdapter = Nothing
+        Me.TableAdapterManager.tempat_tidurTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        Me.TableAdapterManager.userTableAdapter = Nothing
+        '
+        'DokterDataGridView
+        '
+        Me.DokterDataGridView.AllowUserToAddRows = False
+        Me.DokterDataGridView.AllowUserToDeleteRows = False
+        Me.DokterDataGridView.AutoGenerateColumns = False
+        Me.DokterDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DokterDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn5})
+        Me.DokterDataGridView.DataSource = Me.DokterBindingSource
+        Me.DokterDataGridView.Location = New System.Drawing.Point(12, 81)
+        Me.DokterDataGridView.Name = "DokterDataGridView"
+        Me.DokterDataGridView.ReadOnly = True
+        Me.DokterDataGridView.Size = New System.Drawing.Size(552, 228)
+        Me.DokterDataGridView.TabIndex = 1
+        '
+        'DokterBindingSource
+        '
+        Me.DokterBindingSource.DataMember = "dokter"
+        Me.DokterBindingSource.DataSource = Me.DBDataSet
+        '
+        'DBDataSet
+        '
+        Me.DBDataSet.DataSetName = "DBDataSet"
+        Me.DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnPilih)
+        Me.GroupBox2.Controls.Add(Me.btnClear)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.tbCari)
+        Me.GroupBox2.Controls.Add(Me.btnCari)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(552, 63)
+        Me.GroupBox2.TabIndex = 17
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Cari dan pilih dokter"
+        '
+        'btnPilih
+        '
+        Me.btnPilih.Location = New System.Drawing.Point(466, 24)
+        Me.btnPilih.Name = "btnPilih"
+        Me.btnPilih.Size = New System.Drawing.Size(75, 23)
+        Me.btnPilih.TabIndex = 20
+        Me.btnPilih.Text = "Pilih"
+        Me.btnPilih.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(362, 24)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(29, 23)
+        Me.btnClear.TabIndex = 19
+        Me.btnClear.Text = "X"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 29)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(162, 13)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Cari berdasarkan SIP atau Nama"
+        '
+        'tbCari
+        '
+        Me.tbCari.Location = New System.Drawing.Point(188, 26)
+        Me.tbCari.Name = "tbCari"
+        Me.tbCari.Size = New System.Drawing.Size(167, 20)
+        Me.tbCari.TabIndex = 5
+        '
+        'btnCari
+        '
+        Me.btnCari.Location = New System.Drawing.Point(397, 24)
+        Me.btnCari.Name = "btnCari"
+        Me.btnCari.Size = New System.Drawing.Size(49, 23)
+        Me.btnCari.TabIndex = 3
+        Me.btnCari.Text = "Cari"
+        Me.btnCari.UseVisualStyleBackColor = True
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "sip"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "No. SIP"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.ReadOnly = True
+        Me.DataGridViewTextBoxColumn1.Width = 80
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "nama_lengkap"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Nama Lengkap"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.ReadOnly = True
+        Me.DataGridViewTextBoxColumn2.Width = 200
+        '
+        'DataGridViewTextBoxColumn5
+        '
+        Me.DataGridViewTextBoxColumn5.DataPropertyName = "spesialis"
+        Me.DataGridViewTextBoxColumn5.HeaderText = "Spesialis"
+        Me.DataGridViewTextBoxColumn5.Name = "DataGridViewTextBoxColumn5"
+        Me.DataGridViewTextBoxColumn5.ReadOnly = True
+        Me.DataGridViewTextBoxColumn5.Width = 200
+        '
+        'PilihDokterForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(577, 321)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.DokterDataGridView)
+        Me.Name = "PilihDokterForm"
+        Me.Text = "Pilih Dokter"
+        CType(Me.DokterDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DokterBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DBDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DBDataSet As Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSet
+    Friend WithEvents DokterBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents DokterTableAdapter As Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSetTableAdapters.dokterTableAdapter
+    Friend WithEvents TableAdapterManager As Sistem_Informasi_Sensus_Harian_Rawat_Inap.DBDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents DokterDataGridView As System.Windows.Forms.DataGridView
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents tbCari As System.Windows.Forms.TextBox
+    Friend WithEvents btnCari As System.Windows.Forms.Button
+    Friend WithEvents btnPilih As System.Windows.Forms.Button
+    Friend WithEvents DataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn5 As System.Windows.Forms.DataGridViewTextBoxColumn
+End Class
---
+++ Sistem Informasi Sensus Harian Rawat Inap/PilihDokterForm.vb
@@ -0,0 +1,52 @@
+﻿Public Class PilihDokterForm
+    Public Property sip As String
+    Public Property nama As String
+
+    Private Sub DokterBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.Validate()
+        Me.DokterBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.DBDataSet)
+    End Sub
+
+    Private Sub PilihDokterForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form_VisibleChanged(ByVal sender As Object, ByVal e As EventArgs) Handles Me.VisibleChanged
+        If Me.Visible Then
+            Tampil()
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        tbCari.Text = ""
+        Cari()
+    End Sub
+
+    Private Sub btnCari_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCari.Click
+        Cari()
+    End Sub
+
+    Private Sub btnPilih_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPilih.Click
+        Me.sip = DokterDataGridView(0, DokterDataGridView.CurrentRow.Index).Value
+        Me.nama = DokterDataGridView(1, DokterDataGridView.CurrentRow.Index).Value
+        Me.DialogResult = Windows.Forms.DialogResult.OK
+    End Sub
+
+    Private Sub Cari()
+        Dim cari As String = tbCari.Text
+        Dim filter As String
+        If cari = "" Then
+            Tampil()
+        Else
+            filter = "deleted_at IS NULL AND (sip = '" & cari & "' OR nama_lengkap LIKE '%" & cari & "%')"
+            Tampil(filter)
+        End If
+    End Sub
+
+    Private Sub Tampil(Optional ByVal filter As String = "deleted_at IS NULL")
+        DokterBindingSource.Filter = filter
+        DokterBindingSource.Sort = "sip"
+        Me.DokterTableAdapter.Fill(Me.DBDataSet.dokter)
+    End Sub
+End Class
